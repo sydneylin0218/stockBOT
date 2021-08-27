cc95865a64a205c9961ea2942f4462b422ff4a0f
Sydney <108206008@g.nccu.edu.tw>
2021-08-27 14:33:27 +0800

    main

---
+++ stockBOT.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki 2.0 Template For Python3
+
+    [URL] https://api.droidtown.co/Loki/BulkAPI/
+
+    Request:
+        {
+            "username": "your_username",
+            "input_list": ["your_input_1", "your_input_2"],
+            "loki_key": "your_loki_key",
+            "filter_list": ["intent_filter_list"] # optional
+        }
+
+    Response:
+        {
+            "status": True,
+            "msg": "Success!",
+            "version": "v223",
+            "word_count_balance": 2000,
+            "result_list": [
+                {
+                    "status": True,
+                    "msg": "Success!",
+                    "results": [
+                        {
+                            "intent": "intentName",
+                            "pattern": "matchPattern",
+                            "utterance": "matchUtterance",
+                            "argument": ["arg1", "arg2", ... "argN"]
+                        },
+                        ...
+                    ]
+                },
+                {
+                    "status": False,
+                    "msg": "No Match Intent!"
+                }
+            ]
+        }
+"""
+from ArticutAPI import Articut
+from requests import post
+from requests import codes
+from bs4 import BeautifulSoup
+import requests
+import json
+import math
+try:
+    from intent import Loki_symbol
+except:
+    from .intent import Loki_symbol
+
+with open("account.info", encoding="utf-8") as f:
+    accountDICT = json.loads(f.read())
+
+LOKI_URL = "https://api.droidtown.co/Loki/BulkAPI/"
+USERNAME = accountDICT["username"]
+LOKI_KEY = accountDICT["loki_key"]
+
+# 意圖過濾器說明
+# INTENT_FILTER = []        => 比對全部的意圖 (預設)
+# INTENT_FILTER = [intentN] => 僅比對 INTENT_FILTER 內的意圖
+INTENT_FILTER = []
+
+articut = Articut(username= accountDICT["username"], apikey= accountDICT["apikey"])
+
+class LokiResult():
+    status = False
+    message = ""
+    version = ""
+    balance = -1
+    lokiResultLIST = []
+
+    def __init__(self, inputLIST, filterLIST):
+        self.status = False
+        self.message = ""
+        self.version = ""
+        self.balance = -1
+        self.lokiResultLIST = []
+        # filterLIST 空的就採用預設的 INTENT_FILTER
+        if filterLIST == []:
+            filterLIST = INTENT_FILTER
+
+        try:
+            result = post(LOKI_URL, json={
+                "username": USERNAME,
+                "input_list": inputLIST,
+                "loki_key": LOKI_KEY,
+                "filter_list": filterLIST
+            })
+
+            if result.status_code == codes.ok:
+                result = result.json()
+                self.status = result["status"]
+                self.message = result["msg"]
+                if result["status"]:
+                    self.version = result["version"]
+                    self.balance = result["word_count_balance"]
+                    self.lokiResultLIST = result["result_list"]
+            else:
+                self.message = "Connect failed."
+        except Exception as e:
+            self.message = str(e)
+
+    def getStatus(self):
+        return self.status
+
+    def getMessage(self):
+        return self.message
+
+    def getVersion(self):
+        return self.version
+
+    def getBalance(self):
+        return self.balance
+
+    def getLokiStatus(self, index):
+        rst = False
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["status"]
+        return rst
+
+    def getLokiMessage(self, index):
+        rst = ""
+        if index < len(self.lokiResultLIST):
+            rst = self.lokiResultLIST[index]["msg"]
+        return rst
+
+    def getLokiLen(self, index):
+        rst = 0
+        if index < len(self.lokiResultLIST):
+            if self.lokiResultLIST[index]["status"]:
+                rst = len(self.lokiResultLIST[index]["results"])
+        return rst
+
+    def getLokiResult(self, index, resultIndex):
+        lokiResultDICT = None
+        if resultIndex < self.getLokiLen(index):
+            lokiResultDICT = self.lokiResultLIST[index]["results"][resultIndex]
+        return lokiResultDICT
+
+    def getIntent(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["intent"]
+        return rst
+
+    def getPattern(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["pattern"]
+        return rst
+
+    def getUtterance(self, index, resultIndex):
+        rst = ""
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["utterance"]
+        return rst
+
+    def getArgs(self, index, resultIndex):
+        rst = []
+        lokiResultDICT = self.getLokiResult(index, resultIndex)
+        if lokiResultDICT:
+            rst = lokiResultDICT["argument"]
+        return rst
+
+def runLoki(inputLIST, filterLIST=[]):
+    resultDICT = {}
+    lokiRst = LokiResult(inputLIST, filterLIST)
+    if lokiRst.getStatus():
+        for index, key in enumerate(inputLIST):
+            for resultIndex in range(0, lokiRst.getLokiLen(index)):
+                # symbol
+                if lokiRst.getIntent(index, resultIndex) == "symbol":
+                    resultDICT = Loki_symbol.getResult(key, lokiRst.getUtterance(index, resultIndex), lokiRst.getArgs(index, resultIndex), resultDICT)
+
+    else:
+        resultDICT = {"msg": lokiRst.getMessage()}
+    return resultDICT
+
+def testLoki(inputLIST, filterLIST):
+    INPUT_LIMIT = 20
+    for i in range(0, math.ceil(len(inputLIST) / INPUT_LIMIT)):
+        resultDICT = runLoki(inputLIST[i*INPUT_LIMIT:(i+1)*INPUT_LIMIT], filterLIST)
+
+
+if __name__ == "__main__":
+    # symbol
+    print("[TEST] symbol")
+    inputLIST = ['台積電基本資料','台積電的成交價','台積電昨日收盤價','台積電的基本資料','關於台積電的資訊','我想知道台積電的基本資料']
+    testLoki(inputLIST, ['symbol'])
+    print("")
+
+    # 輸入其它句子試看看
+    inputLIST = ["2330的基本資料"]
+    filterLIST = []
+    resultDICT = runLoki(inputLIST, filterLIST)
+    print("Result => {}".format(resultDICT))
+    
+    print("resultDICT")
+    
+    #產業別爬蟲
+    URL = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID="+ resultDICT["symbol"]
+    print(URL)
+    
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+    
+    r = requests.post(url=URL,headers=headers)
+    
+    html =BeautifulSoup(r.content, "html.parser")
+    
+    #基本資料__名稱
+    
+    table = html.findAll("table")[40]
+    
+    table_row_name=table.findAll("tr")[1]
+    td_name = table_row_name.findAll("td")[1]
+    
+    name = td_name.text
+    resultDICT["name"] = name
+    
+    #基本資料__產業別
+    
+    table_row_industry=table.findAll("tr")[2]
+    
+    td_industry=table_row_industry.findAll("td")[1]
+
+    industry=td_industry.text
+    
+    #industry寫入resultDICT
+    
+    resultDICT["industry"] = industry
+    
+    
+    
+    #基本資料__市值
+    table_row_value=table.findAll("tr")[4]
+ 
+    td_value = table_row_value.findAll("td")[3]
+
+    value = td_value.text
+    resultDICT["value"] = value    
+    
+    #基本資料_主要業務
+    
+    table_row_business=table.findAll("tr")[14]
+
+    td_business = table_row_business.findAll("td")[0]
+
+    business = td_business.text
+    
+    resultDICT["business"] = business
+    #resultDICT
+    print(resultDICT) 
+    
+    
+    
+    
+    
