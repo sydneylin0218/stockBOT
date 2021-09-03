0c9a7d60822596d5b6e32713e0c9df05a33b6646
Sydney <108206008@g.nccu.edu.tw>
2021-09-03 18:07:49 +0800

    growth function

--- fc_info.py
+++ fc_info.py
@@ -9,7 +9,6 @@ from requests import codes
 
 def information(symbol):
     URL = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID="+ symbol 
-    print(URL)
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
     r = requests.post(url=URL,headers=headers)
     html =BeautifulSoup(r.content, "html.parser")
@@ -41,4 +40,30 @@ def information(symbol):
     result_infoDICT["business"] = business    
     
     return result_infoDICT
+    
+
+def growth(symbol):
+    URL = "https://goodinfo.tw/StockInfo/StockFinDetail.asp?RPT_CAT=XX_M_QUAR_ACC&STOCK_ID="+ symbol 
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+    r = requests.post(url=URL,headers=headers)
+    html =BeautifulSoup(r.content, "html.parser")
+    
+    result_growthDICT = {}
+    
+    table = html.findAll("table")[16]
+    table_row_revenue=table.findAll("tr")[14]
+    td_revenue = table_row_revenue.findAll("td")[1]
+    revenue_YOY = td_revenue.text
+    result_growthDICT["revenue_YOY"] = revenue_YOY
+    
+    table_row_growth_profit = table.findAll("tr")[15]
+    td_growth_profit = table_row_growth_profit.findAll("td")[1]
+    growth_profit_YOY = td_growth_profit.text
+    result_growthDICT["growth_profit_YOY"] = growth_profit_YOY
+    
+
+    table_row_operating_income=table.findAll("tr")[16]
+    td_operating_income = table_row_operating_income.findAll("td")[1]
+    operating_income_YOY = td_operating_income.text
+    print(operating_income_YOY)    
     
