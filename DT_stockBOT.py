a4be6b689b71f2a71dcb6a2379a8bdf3f2c155e3
Sydney <108206008@g.nccu.edu.tw>
2021-09-05 19:29:38 +0800

    連接discord

--- DT_stockBOT.py
+++ DT_stockBOT.py
@@ -4,8 +4,14 @@
 import discord
 import json
 
+
 with open("account.info", encoding="utf-8") as f:
     accountDICT = json.loads(f.read())
+    
+from ArticutAPI import Articut
+articut = Articut(username= accountDICT["username"], apikey= accountDICT["apikey"])
+
+from stockBOT import runLoki
 
 class BotClient(discord.Client):
     async def on_ready(self):
@@ -26,6 +32,31 @@ class BotClient(discord.Client):
                 await message.reply('pong')
             if msg == 'ping ping':
                 await message.reply('pong pong')
+            else:
+                inputLIST = ["聯發科的基本資料"]
+                filterLIST = []
+                resultDICT = runLoki(inputLIST, filterLIST)
+                print("Result => {}".format(resultDICT))
+                
+                print(resultDICT)
+                
+                if resultDICT["function"] == "information":
+                    result_infoDICT = information(resultDICT["symbol"])
+                    resultDICT.update(result_infoDICT)
+                elif resultDICT["function"] == "growth":
+                    result_growthDICT = growth(resultDICT["symbol"])
+                    resultDICT.update(result_growthDICT)
+                    
+            
+                if resultDICT["function"] == "information":
+                    print(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的公司基本資料如下！"+"\n公司名稱："+resultDICT["name"]+"\n產業別："+resultDICT["industry"]+"\n市值"+resultDICT["value"]+"\n主要業務："+resultDICT["business"])  
+                elif resultDICT["function"] == "information":
+                    print(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的公司基本資料如下！"+"\n公司名稱："+resultDICT["name"]+"\n產業別："+resultDICT["industry"]+"\n市值"+resultDICT["value"]+"\n主要業務："+resultDICT["business"])    
+                elif resultDICT["symbol"] == None:
+                    print("不確定您要找哪一支股票的資訊！請再輸入一次股票名稱或是代號！")
+                elif resultDICT["function"] == None:
+                    print("不確定您要找哪一類的資訊！請再輸入一次要查的資料類別！")
+                                    
 
 if __name__ == "__main__":
     client = BotClient()
