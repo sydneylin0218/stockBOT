359cc2ed0488b1a0dd294ddae583e5fb0e3c5648
Sydney <108206008@g.nccu.edu.tw>
2021-09-12 22:21:52 +0800

    stockBOT update

--- stockBOT_Discord.py
+++ stockBOT_Discord.py
@@ -7,6 +7,7 @@ import logging
 import re
 from fc_info import information
 from fc_info import growth
+from fc_info import safety
 from DICT import companyDICT
 
 with open("account.info", encoding="utf-8") as f:
@@ -46,28 +47,33 @@ class BotClient(discord.Client):
                 
                 print(resultDICT)
                 
-                if  resultDICT["fun_information"] == True:
+                if  "fun_information" in resultDICT.keys():
                     result_infoDICT = information(resultDICT["symbol"])
                     resultDICT.update(result_infoDICT)
-                elif resultDICT["fun_growth"] == True:
+                elif "fun_growth" in resultDICT.keys():
                     result_growthDICT = growth(resultDICT["symbol"])
                     resultDICT.update(result_growthDICT)
-                elif resultDICT["fun_profitability"] ==True:
+                elif "fun_profitability" in resultDICT.keys():
                     result_profitabilityDICT = profitability(resultDICT["symbol"])
-                    resultDICT.update(result_profitabilityDICT)                
+                    resultDICT.update(result_profitabilityDICT)   
+                elif "fun_safety" in resultDICT.keys():
+                    result_safetyDICT = safety(resultDICT["symbol"])
+                    resultDICT.update(result_safetyDICT)                       
                     
                     
             
-                if resultDICT["fun_information"] == True:
-                    await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的公司基本資料如下！"+"\n公司名稱："+resultDICT["name"]+"\n產業別："+resultDICT["industry"]+"\n市值："+resultDICT["value"]+"\n主要業務："+resultDICT["business"])  
-                elif resultDICT["fun_growth"] == True:
-                    await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的獲利年成長率如下！"+"\n營收年成長率："+resultDICT["revenue_YOY"]+"\n毛利年成長率："+resultDICT["gross_profit_YOY"]+"\n營業利益年成長率："+resultDICT["operating_income_YOY"]+"\n稅前淨利年成長率："+resultDICT["NIBT_YOY"]+"\n稅後淨利年成長率："+resultDICT["NI_YOY"]+"\n每股稅後盈餘年成長率："+resultDICT["EPS_YOY"]) 
-                elif resultDICT["fun_growth"] == True:
-                    await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的本季獲利情形如下！"+"\n營收年成長率："+resultDICT["revenue_YOY"]+"\n毛利年成長率："+resultDICT["gross_profit_YOY"]+"\n營業利益年成長率："+resultDICT["operating_income_YOY"]+"\n稅前淨利年成長率："+resultDICT["N                IBT_YOY"]+"\n稅後淨利年成長率："+resultDICT["NI_YOY"]+"\n每股稅後盈餘年成長率："+resultDICT["EPS_YOY"]) 
-                elif resultDICT["symbol"] == None:
-                    await message.reply("不確定您要找哪一支股票的資訊！請再輸入一次股票名稱或是代號！")
-                else:
-                    await message.reply("不確定您要找哪一類的資訊！請再輸入一次要查的資料類別！")
+                    if "fun_information" in resultDICT.keys():
+                        await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"的公司基本資料如下！"+"\n公司名稱："+resultDICT["name"]+"\n產業別："+resultDICT["industry"]+"\n市值："+resultDICT["value"]+"\n主要業務："+resultDICT["business"])  
+                    elif "fun_growth" in resultDICT.keys():
+                        await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"在"+resultDICT["quarter"]+"的獲利年成長率如下！"+"\n營收年成長率："+resultDICT["revenue_YOY"]+"\n毛利年成長率："+resultDICT["gross_profit_YOY"]+"\n營業利益年成長率："+resultDICT["operating_income_YOY"]+"\n稅前淨利年成長率："+resultDICT["NIBT_YOY"]+"\n稅後淨利年成長率："+resultDICT["NI_YOY"]+"\n每股稅後盈餘年成長率："+resultDICT["EPS_YOY"])    
+                    elif "fun_profitability" in resultDICT.keys():
+                        await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"在"+resultDICT["quarter"]+"的償債能力如下！"+"\n營業毛利率："+resultDICT["GPM"]+"\n營業利益率："+resultDICT["OPM"]+"\n稅前淨利率："+resultDICT["PTPM"]+"\n稅後淨利率："+resultDICT["NPM"]+"\n每股稅後盈餘："+resultDICT["EPS"]+"\n每股淨值(元)："+resultDICT["NASPS"]+"\n股東權益報酬率："+resultDICT["ROE"]+"\n資產報酬率："+resultDICT["ROA"])    
+                    elif "fun_safety" in resultDICT.keys():
+                        await message.reply(companyDICT[resultDICT["symbol"]][0]+resultDICT["symbol"]+"在"+resultDICT["quarter"]+"的償債能力如下！"+"\n現金比："+resultDICT["CR"]+"\n速動比："+resultDICT["QR"]+"\n流動比："+resultDICT["current_ratio"]+"\n利息保障倍數："+resultDICT["ICR"]+"\n現金流量比："+resultDICT["OCFR"]+"\n負債總額比(元)："+resultDICT["DR"])    
+                    elif resultDICT["symbol"] == None:
+                        await message.reply("不確定您要找哪一支股票的資訊！請再輸入一次股票名稱或是代號！")
+                    else:
+                        await message.reply("不確定您要找哪一類的資訊！請再輸入一次要查的資料類別！")
                                     
 
 if __name__ == "__main__":
